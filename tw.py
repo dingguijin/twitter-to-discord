3c664d8a14679bab2007732f93eeb9b13fc0c660
Guijin Ding <dingguijin@gmail.com>
2023-04-01 23:06:47 +0800

    random scroll

--- tw.py
+++ tw.py
@@ -21,6 +21,16 @@ def up_down(driver):
     ag.scroll(1000)
     return
 
+def up_down_with_se(driver):
+    driver.execute_script('return window.scrollTo(0, 12);')
+    time.sleep(2);
+    driver.execute_script('return window.scrollTo(0, 1280);')
+    time.sleep(1);
+
+    driver.execute_script('return window.scrollTo(0, 12);')
+
+    return
+
 def get_driver_instance():
     options = webdriver.ChromeOptions()
     options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
@@ -39,13 +49,14 @@ def get_page(driver, url):
             frame = driver.find_element(By.CSS_SELECTOR, "#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-14lw9ot.r-jxzhtn.r-1ljd8xs.r-13l2t4g.r-1phboty.r-16y2uox.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div.css-1dbjc4n.r-1jgb5lz.r-1ye8kvj.r-13qz1uu > div > section > div > div > div:nth-child(1) > div > div > div > div > span")
         except Exception as e:
             time.sleep(10)
-            up_down(driver)
+            up_down_with_se(driver)
             continue
         if frame:
             print(frame)
+            time.sleep(1)
             frame.click()
             time.sleep(10)
-            up_down(driver)
+            up_down_with_se(driver)
 
 
     return
