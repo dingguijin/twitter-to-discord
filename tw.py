8067430ed395b0ef7787dfbdf0ddeeb7df267d39
Guijin Ding <dingguijin@gmail.com>
2023-04-22 09:38:41 +0800

    all ex

--- tw.py
+++ tw.py
@@ -17,14 +17,17 @@ def restart():
 
 def print_tweet(driver):
     es = driver.find_elements(By.CSS_SELECTOR, '[data-testid="tweet"]')
-    if es:
-        for e in es:
-            _t = e.find_element(By.CSS_SELECTOR, 'div[data-testid="tweetText"]')
-            print(_t.text)
-            print(_t.id)
+    if not es:
+        return
+    for e in es:
+        _t = e.find_element(By.CSS_SELECTOR, 'div[data-testid="tweetText"]')
+        print(_t)
+        print(_t.text)
+        print(_t.id)
     return
 
 def scroll_with_se(driver, offset):
+    time.sleep(1)
     driver.execute_script('return window.scrollTo(0, %d);' % offset)
     print_tweet(driver)
     return
@@ -39,13 +42,9 @@ def up_down_with_se(driver):
         time.sleep(abs(_t));
 
     scroll_with_se(driver, 12)
-    try:
-        ele = driver.find_element(By.XPATH, "//*[@id=\"react-root\"]/div/div/div[2]/header/div/div/div/div[1]/div[2]/nav/a[1]")
-        if ele:
-            ele.click()
-    except Exception as e:
-        print("Exception .... ", e)
-        restart()
+    ele = driver.find_element(By.XPATH, "//*[@id=\"react-root\"]/div/div/div[2]/header/div/div/div/div[1]/div[2]/nav/a[1]")
+    if ele:
+        ele.click()
     return
 
 def get_driver_instance():
@@ -62,28 +61,15 @@ def get_page(driver, url):
     time.sleep(1.0)
     time.sleep(10)
     while True:
-        try:
-            frame = driver.find_element(By.CSS_SELECTOR, "#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-14lw9ot.r-jxzhtn.r-1ljd8xs.r-13l2t4g.r-1phboty.r-16y2uox.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div.css-1dbjc4n.r-1jgb5lz.r-1ye8kvj.r-13qz1uu > div > section > div > div > div:nth-child(1) > div > div > div > div > span")
-        except Exception as e:
-            time.sleep(10)
-            try:
-                up_down_with_se(driver)
-            except Exception as e:
-                print("Exception .... ", e)
-                restart()
-
+        frame = driver.find_element(By.CSS_SELECTOR, "#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div.css-1dbjc4n.r-14lw9ot.r-jxzhtn.r-1ljd8xs.r-13l2t4g.r-1phboty.r-16y2uox.r-1jgb5lz.r-11wrixw.r-61z16t.r-1ye8kvj.r-13qz1uu.r-184en5c > div > div.css-1dbjc4n.r-1jgb5lz.r-1ye8kvj.r-13qz1uu > div > section > div > div > div:nth-child(1) > div > div > div > div > span")
+        if not frame:
+            up_down_with_se(driver)
+            time.sleep(5)
             continue
-        if frame:
-            print(frame)
-            time.sleep(1)
-            try:
-                frame.click()
-                time.sleep(abs(random.gauss(10.0, 8.0)))
-                up_down_with_se(driver)
-            except Exception as e:
-                print("Exception .... ", e)
-                restart()
-
+        time.sleep(1)
+        frame.click()
+        time.sleep(abs(random.gauss(10.0, 8.0)))
+        up_down_with_se(driver)
 
     return
 
@@ -91,4 +77,8 @@ def get_page(driver, url):
 if __name__ == "__main__":
     driver = get_driver_instance()
     url = "https://twitter.com/home"
-    s = get_page(driver, url)
+    try:
+        s = get_page(driver, url)
+    except Exception as e:
+        print(e)
+        _restart()
