d928b263b6d730a6ded8815ae5121edd65df4652
Guijin Ding <dingguijin@gmail.com>
2023-04-24 13:46:58 +0800

    sleep 600

--- monitor_chrome.py
+++ monitor_chrome.py
@@ -9,12 +9,11 @@ def monitor_chrome():
                 window = pyautogui.getWindowsWithTitle(title)[0]
                 window.activate()
 
-                if pyautogui.locateOnScreen('error.png', grayscale=True, confidence=0.9) is not None:
-                    print("Something went wrong while displaying this page.")
-                    pyautogui.hotkey('ctrl', 'r')
+                #if pyautogui.locateOnScreen('error.png', grayscale=True, confidence=0.9) is not None:
+                #    print("Something went wrong while displaying this page.")
+                pyautogui.hotkey('ctrl', 'r')
 
-                break
-        time.sleep(1)
+        time.sleep(600)
 
 monitor_chrome()
 
