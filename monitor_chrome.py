2743fd1b21ff38e7ca241ea918cbcd51ea7db8f7
Guijin Ding <dingguijin@gmail.com>
2023-04-26 23:21:34 +0800

    monitor

--- monitor_chrome.py
+++ monitor_chrome.py
@@ -3,22 +3,20 @@ import time
 import datetime
 
 def monitor_chrome():
-    while True:
-        titles = pyautogui.getAllTitles()
-        
-        print(datetime.datetime.now())
-        print(titles)
+    titles = pyautogui.getAllTitles()
+    
+    print(datetime.datetime.now())
+    print(titles)
 
-        for title in titles:
-            if "Chrome" in title:
-                window = pyautogui.getWindowsWithTitle(title)[0]
-                window.activate()
+    for title in titles:
+        if "Chrome" in title:
+            window = pyautogui.getWindowsWithTitle(title)[0]
+            window.activate()
 
-                #if pyautogui.locateOnScreen('error.png', grayscale=True, confidence=0.9) is not None:
-                #    print("Something went wrong while displaying this page.")
-                pyautogui.hotkey('ctrl', 'r')
+            #if pyautogui.locateOnScreen('error.png', grayscale=True, confidence=0.9) is not None:
+            #    print("Something went wrong while displaying this page.")
+            pyautogui.hotkey('ctrl', 'r')
 
-        time.sleep(600)
 
 monitor_chrome()
 
